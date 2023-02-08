2a96985a4fc81374b9e5c7f14972846b4e0b2f2c
wealthy <thaigiauvlvn@gmail.com>
2023-02-08 15:40:03 +0700

    Cai dat cac handler truy xuat CSDL

---
+++ app/services/contact.service.js
@@ -0,0 +1,81 @@
+const { ObjectId } = require("mongodb");
+
+class ContactService {
+    constructor(client) {
+        this.Contact = client.db().collection("contacts");
+    }
+
+    extractConactData(payload) {
+        const contact = {
+            name: payload.name,
+            email: payload.email,
+            adress: payload.adress,
+            phone: payload.phone,
+            favorite: payload.favorite,
+        };
+        Object.keys(contact).forEach(
+            (key) => contact[key] === undefined && delete contact[key]
+        );
+
+        return contact;
+    }
+
+    async create(payload) {
+        const contact = this.extractConactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            contact,
+            { $set: { favorite: contact.favorite === true } },
+            { returnDocument: "after", upsert: true }
+        );
+        return result.value
+    }
+
+    async find(filter) {
+        const cursor = await this.Contact.find(filter);
+        return await cursor.toArray();
+    }
+
+    async findByName(name) {
+        return await this.find({
+            name: { $regex: new RegExp(name), $options: "i" },
+        });
+    }
+
+    async findById(id) {
+        return await this.Contact.findOne({
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        });
+    }
+
+    async update(id, payload) {
+        const filter = {
+            _id: ObjectId.isValid(id) ? new ObjectId(id) : null,
+        };
+        const update = this.extractConactData(payload);
+        const result = await this.Contact.findOneAndUpdate(
+            filter,
+            { $set: update },
+            { returnDocument: "after" }
+        );
+        return result.value;
+    }
+
+    async delete(id) {
+        const result = await this.Contact.findOneAndDelete({
+            _id: ObjectId.isValid(id) ? new ObjectId : null,
+        });
+        return result.value;
+    }
+
+    async findAllFavorite() {
+        return await this.find({ favorite: true });
+    }
+
+    async deleteAll() {
+        const result = await this.Contact.deleteMany({});
+        return result.deleteCount;
+    }
+
+}
+
+module.exports = ContactService;
